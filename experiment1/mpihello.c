012d3c88f3ccb4859198f76cbeb22479c5308c1d
Lunt <bjlunt2@sp19-cs484-001.cs.illinois.edu>
2019-05-08 09:04:51 -0500

    oops

--- experiment1/mpihello.c
+++ experiment1/mpihello.c
@@ -5,7 +5,7 @@ int main(int argc, char** argv) {
 	// Initialize the MPI environment
 	MPI_Init(NULL, NULL);
 	int my_rank, world_size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	
 	printf("Hello world %d of %d",my_rank, world_size);
